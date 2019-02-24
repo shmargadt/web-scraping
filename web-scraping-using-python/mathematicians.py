6f74e05d93b2d0700e8b060d31326c72db2d13b8
Tomer Shmargad <tomer.shmargad@bluevine.com>
2019-02-24 21:11:27 +0200

    adding network helper

---
+++ web-scraping-using-python/mathematicians.py
@@ -0,0 +1,24 @@
+from bs4 import BeautifulSoup
+from networkHelper import simple_get
+
+def get_names():
+    """
+    Downloads the page where the list of mathematicians is found
+    and returns a list of strings, one per mathematician
+    """
+    url = 'http://www.fabpedigree.com/james/mathmen.htm'
+    response = simple_get(url)
+
+    if response is not None:
+        html = BeautifulSoup(response, 'html.parser')
+        names = set()
+        for li in html.select('li'):
+            for name in li.text.split('\n'):
+                if len(name) > 0:
+                    names.add(name.strip())
+        return list(names)
+
+    # Raise an exception if we failed to get any data from the url
+    raise Exception('Error retrieving contents at {}'.format(url))
+
+print(get_names())
